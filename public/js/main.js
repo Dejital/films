99764912798d7ede49bd87356705bdcf49fd6765
Serge Nevsky <sergey@sergeyk.com>
2015-10-01 21:37:49 -0500

    Add sorting to table by clicking on column header.

--- public/js/main.js
+++ public/js/main.js
@@ -17,6 +17,13 @@ app.controller('FilmsController', ['$scope', '$http', FilmsController]);
 module.exports = function($scope, $http) {
     $http.get('data/data.json').success(function (data){
         $scope.films = data;
+        $scope.reverseSort = false;
+        $scope.predicate = null;
+        
+        $scope.order = function (predicate) {
+            $scope.reverseSort = ($scope.predicate === predicate) ? !$scope.reverseSort : false;
+            $scope.predicate = predicate;
+        }
     });
 };
 
