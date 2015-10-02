30fc693fb5edd40d1640f19d5449e74623cd2172
Serge Nevsky <sergey@sergeyk.com>
2015-10-01 21:52:55 -0500

    Fix sorting by date.

--- public/js/main.js
+++ public/js/main.js
@@ -15,15 +15,18 @@ app.controller('FilmsController', ['$scope', '$http', FilmsController]);
  */
 
 module.exports = function($scope, $http) {
+    $scope.reverseSort = true;
+    $scope.predicate = null;
+    $scope.order = function (predicate) {
+        $scope.reverseSort = ($scope.predicate === predicate) ? !$scope.reverseSort : false;
+        $scope.predicate = predicate;
+    }
+
     $http.get('data/data.json').success(function (data){
         $scope.films = data;
-        $scope.reverseSort = false;
-        $scope.predicate = null;
-        
-        $scope.order = function (predicate) {
-            $scope.reverseSort = ($scope.predicate === predicate) ? !$scope.reverseSort : false;
-            $scope.predicate = predicate;
-        }
+        angular.forEach($scope.films, function(film) {
+            film.dateSeen = new Date(film.dateSeen);
+        });
     });
 };
 
